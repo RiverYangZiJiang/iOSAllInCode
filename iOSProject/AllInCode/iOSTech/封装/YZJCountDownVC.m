7d6a804f73770b0c8c82ef13b0d97f69d175ba69
RiverYang <riveryzj@163.com>
2021-01-12 06:31:24 +0800

    倒计时

---
+++ iOSProject/AllInCode/iOSTech/封装/YZJCountDownVC.m
@@ -0,0 +1,32 @@
+//
+//  YZJCountDownVC.m
+//  AllInCode
+//
+//  Created by 杨子江 on 1/12/21.
+//  Copyright © 2021 github.com/njhu. All rights reserved.
+//
+
+#import "YZJCountDownVC.h"
+
+@interface YZJCountDownVC ()
+
+@end
+
+@implementation YZJCountDownVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
